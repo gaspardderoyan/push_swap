2d9504c136e060eba7f24144dc8add39b74cc907
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2024-12-18 17:42:54 +0000

    fixed issues, got a working list

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include "../libft/inc/libft.h"
 
+void    add_nbr_to_lst(t_list **lst, int n)
+{
+    int *new;
+    new = (int *)malloc(sizeof(int));
+    if (!new)
+        return ;
+    *new = n;
+    ft_lstadd_front(lst, ft_lstnew(new));
+}
+
 void    lst_from_str(char *str, t_list **lst)
 {
     int     cur_num;
@@ -10,30 +20,43 @@ void    lst_from_str(char *str, t_list **lst)
     {
         if (ft_isdigit(*str) == 0)
             str++;
-        else 
-        {
-            start = str;
-            while (ft_isdigit(*str) && *str)
-                str++;
-            cur_str = ft_substr(start, 0, str - start); 
-            cur_num = ft_atoi(cur_str);
-            ft_lstadd_front(lst, ft_lstnew(&cur_num));
-            printf("%i\n", *(int *)(*lst)->content);
-            free(cur_str);
+        if (*str == '\0')
+            break ;
+        start = str;
+        while (*str && ft_isdigit(*str))
             str++;
-        }
+        cur_str = ft_substr(start, 0, str - start);
+        cur_num = ft_atoi(cur_str); 
+        free(cur_str);
+        add_nbr_to_lst(lst, cur_num);
     }
 }
 
-void    print_node(void *n)
+
+void print_list(t_list *lst)
 {
-    printf("%i\n", *(int *)n);
+    t_list *current;
+
+    while (lst != NULL)
+    {
+        if (lst->content != NULL)
+            printf("contents: %d\n", *(int *)lst->content);
+        else
+            printf("content is NULL\n");
+        current = lst;
+        lst = lst->next;
+        free(current->content);
+        free(current);
+    }
 }
+
+
 
 int main(int argc, char **argv)
 {
     t_list *a;
 
+    a = NULL;
     if (argc == 1)
         ft_putstr_fd("Error\n", 2);
     else if (argc == 2)
@@ -41,7 +64,7 @@ int main(int argc, char **argv)
         ft_putstr_fd(argv[1], 1);
         ft_putstr_fd("\n", 1);
         lst_from_str(argv[1], &a);
-        // ft_lstiter(a, &print_node);
+        print_list(a);
     }
 
     return (0);
