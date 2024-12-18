08a8b285fb137905699a11eb1de2e23fb6026f22
Gaspard Deroyan <gaspard.deroyan@gmail.com>
2024-12-18 23:40:35 +0000

    idk

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include "../libft/inc/libft.h"
+#include "double_lst.h"
+#include <errno.h>
 
-void    add_nbr_to_lst(t_list **lst, int n)
+void    add_nbr_to_lst(t_dlist **lst, int n)
 {
     int *new;
     new = (int *)malloc(sizeof(int));
     if (!new)
         return ;
     *new = n;
-    ft_lstadd_back(lst, ft_lstnew(new));
+    ft_dlstadd_back(lst, ft_dlstnew(new));
 }
 
-void    lst_from_str(char *str, t_list **lst)
+void    lst_from_str(char *str, t_dlist **lst)
 {
     int     cur_num;
     char    *cur_str;
     char    *start;
+    char    *endptr;
 
     while (*str)
     {
@@ -26,15 +29,18 @@ void    lst_from_str(char *str, t_list **lst)
         while (*str && ft_isdigit(*str))
             str++;
         cur_str = ft_substr(start, 0, str - start);
-        cur_num = ft_atoi(cur_str); 
+        errno = 0;
+        cur_num = strtol(cur_str, &endptr, 10); 
+        if (errno)
+            printf("ERRIR") ;
         free(cur_str);
         add_nbr_to_lst(lst, cur_num);
     }
 }
 
-void pnf_list(t_list *lst, int free_flag)
+void pnf_list(t_dlist *lst, int free_flag)
 {
-    t_list *current;
+    t_dlist *current;
 
     while (lst != NULL)
     {
@@ -53,7 +59,7 @@ void pnf_list(t_list *lst, int free_flag)
     printf("\n");
 }
 
-void swap_first(t_list **lst)
+void swap_first(t_dlist **lst)
 {
     void *temp;
 
@@ -64,15 +70,15 @@ void swap_first(t_list **lst)
     (*lst)->content = temp;
 }
 
-void    f_both(t_list **a, t_list **b, void (f)(t_list**))
+void    f_both(t_dlist **a, t_dlist **b, void (f)(t_dlist**))
 {
     f(a);
     f(b);
 }
 
-void    push_first(t_list **in, t_list **out)
+void    push_first(t_dlist **in, t_dlist **out)
 {
-    t_list *temp;
+    t_dlist *temp;
 
     if (!in || !*in || !(*in)->content)
         return ;
@@ -80,37 +86,37 @@ void    push_first(t_list **in, t_list **out)
     temp = *in;
     *in = (*in)->next;
     temp->next = NULL;
-    ft_lstadd_front(out, temp);
+    ft_dlstadd_front(out, temp);
 }
 
-void   rotate(t_list **lst)
+void   rotate(t_dlist **lst)
 {
-    t_list *temp;
+    t_dlist *temp;
 
     temp = *lst;
     *lst = (*lst)->next;
     temp->next = NULL;
-    ft_lstadd_back(lst, temp);
+    ft_dlstadd_back(lst, temp);
 }
 
-t_list *get_ntl(t_list *lst)
+t_dlist *get_ntl(t_dlist *lst)
 {
-    t_list *cursor;
+    t_dlist *cursor;
 
     if (lst == NULL || lst->next == NULL)
         return (NULL);
-    cursor = lst;
-    while (cursor->next->next != NULL)
-        cursor = cursor->next;
-    return (cursor);
+    cursor = ft_dlstlast(lst)->prev;
+    if (cursor)
+        return (cursor);
+    return (NULL);
 }
 
-void    reverse(t_list **lst)
+void    reverse(t_dlist **lst)
 {
-    t_list *next_to_last;
-    t_list *last;
+    t_dlist *next_to_last;
+    t_dlist *last;
 
-    last = ft_lstlast(*lst);
+    last = ft_dlstlast(*lst);
     next_to_last = get_ntl(*lst);
     next_to_last->next = NULL;
     last->next = *lst;
@@ -119,15 +125,24 @@ void    reverse(t_list **lst)
 
 int main(int argc, char **argv)
 {
-    t_list *a;
-    t_list *b;
+    t_dlist *a;
+    t_dlist *b;
 
     a = NULL;
     b = NULL;
+    errno = 0;
     if (argc == 1)
         ft_putstr_fd("Error\n", 2);
     else if (argc == 2)
         lst_from_str(argv[1], &a);
+    if (errno)
+    {
+        printf("ERROR\n");
+        return (0);
+    }
+    b = get_ntl(a);
+    ft_putstr_fd("list b:\n", 1);
+    pnf_list(b, 0);
     ft_putstr_fd("list a:\n", 1);
     pnf_list(a, 1);
     return (0);
